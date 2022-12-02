ef9f5e3a11b583d26932c18c8db25150be12491e
Nestor Cano <nestcano2971@gmail.com>
2022-12-02 15:14:30 -0500

    Tiles and tilemap created

---
+++ Assets/Grid.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+
+    [SerializeField] int height;
+    [SerializeField] int lenght;
+
+    bool[,];
+
+
+}
